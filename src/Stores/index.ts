8c2e8fa05e22fc299c35327b2452505bf107b489
OmDutaa <85216552+DutaKey@users.noreply.github.com>
2025-03-08 11:19:51 +0700

    refactor(events): improve type definition for event handlers

--- src/Stores/index.ts
+++ src/Stores/index.ts
@@ -1,6 +1,7 @@
-import { EventMapKey } from "../Types/Event";
+import { WASocket } from "@whiskeysockets/baileys";
+import { EventMap, EventMapKey } from "../Types/Event";
 import { SessionStoreType } from "../Types/Session";
 
 export const sessions: Map<String, SessionStoreType> = new Map();
 
-export const events: Map<EventMapKey, Function> = new Map();
+export const events: Map<EventMapKey, (data: EventMap[EventMapKey], sessionId: string, sock?: WASocket) => any> = new Map();
