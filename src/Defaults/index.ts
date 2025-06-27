74357c689766a5b48cd05b357d45a98dd94ea8d7
OmDutaa <85216552+DutaKey@users.noreply.github.com>
2025-06-27 22:43:11 +0700

    refactor(default): change local_connection_path

--- src/Defaults/index.ts
+++ src/Defaults/index.ts
@@ -1,10 +1,12 @@
+import { Browsers } from '@whiskeysockets/baileys';
 import { SockConfig } from '../Types/Session';
 
-export const LOCAL_CONNECTION_PATH = 'whatsmulti_session';
+export const LOCAL_CONNECTION_PATH = 'whatsmulti_sessions';
 
 export const DEFAULT_CONNECTION_TYPE = 'local';
 
 export const DEFAULT_SOCKET_CONFIG: Partial<SockConfig> = {
+    browser: Browsers.macOS('WhatsMulti'),
     connectTimeoutMs: 30000,
     keepAliveIntervalMs: 60000,
     emitOwnEvents: true,
