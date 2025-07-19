ca2383cbbdf423aaade80291bcd22d77c34c0397
OmDutaa <85216552+DutaKey@users.noreply.github.com>
2025-07-19 23:38:09 +0700

    refactor(default): simply default socket config

--- src/Defaults/index.ts
+++ src/Defaults/index.ts
@@ -1,4 +1,3 @@
-import { Browsers } from '@whiskeysockets/baileys';
 import { SockConfig } from '../Types/Session';
 
 export const LOCAL_CONNECTION_PATH = 'whatsmulti_sessions';
@@ -6,11 +5,5 @@ export const LOCAL_CONNECTION_PATH = 'whatsmulti_sessions';
 export const DEFAULT_CONNECTION_TYPE = 'local';
 
 export const DEFAULT_SOCKET_CONFIG: Partial<SockConfig> = {
-    browser: Browsers.macOS('WhatsMulti'),
-    connectTimeoutMs: 30000,
-    keepAliveIntervalMs: 60000,
-    emitOwnEvents: true,
-    fireInitQueries: true,
-    qrTimeout: 60000,
-    qrMaxWaitMs: 180000,
+    browser: ['WhatsMulti', 'Chrome', '1.5.0'],
 };
